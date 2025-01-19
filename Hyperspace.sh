349a2cec89bad94a5c9a527999578479e40e74ce
0xqianyi <homedali01@163.com>
2025-01-19 11:37:44 +0800

    Hyperspace.sh

--- Hyperspace.sh
+++ Hyperspace.sh
@@ -152,7 +152,12 @@ function deploy_hyperspace_node() {
 
     # 提示用户选择等级
     echo "请选择节点等级（1-5）："
-    select tier in 1/30GB 2/20GB 3/8GB 4/4GB 5/2GB; do
+    echo "1. 30GB"
+    echo "2. 20GB"
+    echo "3. 8GB"
+    echo "4. 4GB"
+    echo "5. 2GB"
+    select tier in 1 2 3 4 5; do
         case $tier in
             1|2|3|4|5)
                 echo "你选择了等级 $tier"
